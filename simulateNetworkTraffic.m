1799855508c687a0bf75b50b4e1db35d26838513
kosiarn <62908964+kosiarn@users.noreply.github.com>
2023-01-04 13:28:43 +0100

    fixed traffic simulation logic
    it now accounts for clients and other network devices connected to the node

--- simulateNetworkTraffic.m
+++ simulateNetworkTraffic.m
@@ -5,11 +5,8 @@ function trafficAmount = simulateNetworkTraffic(G, root_node, current_node , tra
 %       root_node - wezel, na ktorym chcemy policzyc ruch
 %       trafficValueMap - mapa hashowa zawierajaca pary wezel: ilosc ruchu
 %       https://www.mathworks.com/help/matlab/ref/containers.map.html
-if trafficValueMap(current_node) ~= 0
-    trafficAmount = trafficValueMap(current_node); % przypadek liscia - mamy ilosc ruchu, zwracamy ja
-else %jesli nie, liczymy sume z ruchu wezlow-dzieci
     childNodes = findChildren(G, root_node, current_node, successors(G, current_node));
-    tAm = 0; % zmienna podreczna - zmienna trafficAmount w petli nie dzialala poprawnie
+    tAm = trafficValueMap(current_node); % zmienna podreczna - zmienna trafficAmount w petli nie dzialala poprawnie
     for i = 1 : length(childNodes)
         tAm = tAm + simulateNetworkTraffic(G, root_node, childNodes{i}, trafficValueMap);
     end
