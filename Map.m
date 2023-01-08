d5b590eea678c5c0fd39b0ce91cc3497ee98c425
szydelko-anna <szydelko.ania@gmail.com>
2023-01-08 14:49:23 +0100

    Poprawa funkcji Map()
    Jeśli w grafie występuje urządzenie bez przypisanej liczby klientów, funkcja nie wyrzuca błędu.

--- Map.m
+++ Map.m
@@ -1,10 +1,18 @@
-function trafficMap = Map(G, clients, traffic)
+function trafficMap = Map(G, clientMap, traffic)
 %Przypisuje urzadzeniom ruch jaki wystepuje
 %   Dane wejsciowe:
 %       G - graf skierowany
-%       clients - lista z liczba klientow
-%       traffic - wartosc uploadu/downloadu od jednego uzytkownika w Mbps
+%       clientMap - mapa liczby klientow na urzadzeniach
+%       traffic - wartosc uploadu/downloadu od jednego klienta w Mbps
     keySet = transpose(G.Nodes.Name);
-    valueSet = clients*traffic;
+    valueSet = zeros(1, length(keySet));
+    clientMapKeys = keys(clientMap);
+
+    for i = 1:length(keySet)
+        if(any(strcmp(clientMapKeys, keySet{i})))
+            valueSet(i) = clientMap(keySet{i})*traffic;
+        end
+    end
+
     trafficMap = containers.Map(keySet,valueSet);
 end
