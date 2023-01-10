050f3495fc266000c0607455ae33123fa82e33a3
kosiarn <62908964+kosiarn@users.noreply.github.com>
2023-01-10 14:04:25 +0100

    stress simulation function bugfix
    it used to chuck all all overloaded nodes into  one map; not anymore

--- simulateNetworkStress.m
+++ simulateNetworkStress.m
@@ -1,6 +1,10 @@
 function [uploadOverflowMap, downloadOverflowMap] = simulateNetworkStress(G, gateway_node, nodeDownloadMap, nodeUploadMap)
     nodeList = keys(nodeDownloadMap);
     initialOverflowMap = containers.Map(nodeList, zeros(1, length(nodeList)));
-    [~, downloadOverflowMap] = checkLinkLoad(G, gateway_node, gateway_node, nodeDownloadMap, initialOverflowMap, "download");
-    [~, uploadOverflowMap] = checkLinkLoad(G, gateway_node, gateway_node, nodeUploadMap, initialOverflowMap, "upload");
+    initialUploadOverflowMap = containers.Map(initialOverflowMap.keys, initialOverflowMap.values);
+    initialDownloadOverflowMap =  containers.Map(initialOverflowMap.keys, initialOverflowMap.values);
+    [~, initialDownloadOverflowMap] = checkLinkLoad(G, gateway_node, gateway_node, nodeDownloadMap, initialDownloadOverflowMap, "download");
+    [~, initialUploadOverflowMap] = checkLinkLoad(G, gateway_node, gateway_node, nodeUploadMap, initialUploadOverflowMap, "upload");
+    uploadOverflowMap =  containers.Map(initialUploadOverflowMap.keys, initialUploadOverflowMap.values);
+    downloadOverflowMap =  containers.Map(initialDownloadOverflowMap.keys, initialDownloadOverflowMap.values);
 end
